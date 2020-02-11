db0fd2cb72c508aad6f40a7808eb01cc287aedef
ysdgroot <yucatan.de.groote@gmail.com>
2020-02-11 15:49:46 +0100

    Verschoning van de code, verwijdering van de vele comments.
    Signed-off-by: ysdgroot <yucatan.de.groote@gmail.com>

--- TestingEvaluationParametersSimulation/TestStepsAndPathsEvaluation.py
+++ TestingEvaluationParametersSimulation/TestStepsAndPathsEvaluation.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
-import math
-from sklearn.linear_model import LinearRegression
 
 
 def read_data(filename):
@@ -11,18 +8,16 @@ def read_data(filename):
     return data
 
 
-filename = '../Test-steps and accuracy-BS-v1.csv'
+filename = 'Test-steps and accuracy-BS-v1.csv'
 data = read_data(filename)
 
 number_paths = data['paths'].unique()
 step_sizes = data['time_step'].unique()
-lst_legend = []
-show_plots = False
+show_plots = True
 
 
-def plot_accuracy_graph(values, accuracy_type, x_name, title, name_x_axis, name_y_axis,
+def plot_accuracy_graph(values, x_name, y_name, title, name_x_axis, name_y_axis,
                         fix_locations_name=None, fixed_value_locations=None):
-    # todo: maken van grafiek met een fixed value (bv path == 100)
     locations = [i for i in range(len(values[x_name]))]
     if fix_locations_name is not None:
         if fixed_value_locations is not None:
@@ -31,7 +26,7 @@ def plot_accuracy_graph(values, accuracy_type, x_name, title, name_x_axis, name_
             raise ValueError
 
     x_values = values[x_name][locations]
-    y_values = values[accuracy_type][locations]
+    y_values = values[y_name][locations]
 
     unique_x = x_values.unique()
     mean_y = []
@@ -48,68 +43,26 @@ def plot_accuracy_graph(values, accuracy_type, x_name, title, name_x_axis, name_
 
 if show_plots:
     # Test function:
-    plot_accuracy_graph(data, "accuracy_normal", "paths", "Performance", "Number of paths",
+    plot_accuracy_graph(data, "paths", "accuracy_normal", "Performance", "Number of paths",
                         "Relative Difference")
+    plt.show()
 
-    plot_accuracy_graph(data, "accuracy_absolute", "paths", "Performance", "number of paths",
+    plot_accuracy_graph(data, "paths", "accuracy_absolute", "Performance", "Number of paths",
                         "Absolute Relative Difference")
-    # plt.show()
+    plt.show()
 
-    plot_accuracy_graph(data, "max_abs_diff", "paths", "Performance", "number of paths",
-                        "Maximum Absolute Relative Difference")
-    # plt.show()
-
-    plot_accuracy_graph(data, "max_abs_diff", "time_step", "Performance", "Number of steps",
-                        "Maximum Absolute Relative Difference")
-    # plt.show()
-
-    plot_accuracy_graph(data, "accuracy_absolute", "time_step", "Performance", "Number of steps",
+    plot_accuracy_graph(data, "time_step", "accuracy_absolute", "Performance", "Number of steps",
                         "Absolute Relative Difference")
-    # plt.show()
+    plt.show()
 
-    # plot_accuracy_graph(data, "min_rel_diff", "time_step")
-    # plot_accuracy_graph(data, "max_rel_diff", "time_step")
-    # unique_paths = data["paths"].unique()
-    # for path in unique_paths:
-    #     plot_accuracy_graph(data, "accuracy_normal", "time_step", "Performance", "Number of steps",
-    #                         "Relative difference",
-    #                         fix_locations_name='paths',
-    #                         fixed_value_locations=path)
-    # plt.show()
+    plot_accuracy_graph(data, "time_step", "accuracy_normal", "Performance", "Number of steps",
+                        "Relative Difference")
+    plt.show()
 
-x_values = "paths"
-# x_values = "time_step"
+    plot_accuracy_graph(data, "time_step", "time", "Time", "Number of steps",
+                        "Time")
+    plt.show()
 
-# y_values = "accuracy_normal"
-# y_values = "accuracy_absolute"
-y_values = "time"
-
-print(data["time"])
-
-# # print(data[x_values])
-# # print(data[y_values])
-plt.scatter(data[x_values], data[y_values])
-plt.show()
-
-# # make graph for each path size vs accuracy
-# for path in number_paths:
-#     time_steps = data['time_step'][data['paths'] == path]
-#     # accuracy_abs = data['accuracy_absolute'][data['paths'] == path]
-#     # p, = plt.plot(time_steps, accuracy_abs)
-#     accuracy = data['accuracy_normal'][data['paths'] == path]
-#     p, = plt.plot(time_steps, accuracy)
-#     lst_legend.append(p)
-# plt.legend(lst_legend, number_paths)
-# plt.show()
-#
-# # make grapf for each time step vs accuracy
-# for stepsize in step_sizes:
-#     locations = data['time_step'] == stepsize
-#     paths = data['paths'][locations]
-#     accuracy = data['accuracy_normal'][locations]
-#     p, = plt.plot(paths, accuracy)
-#
-# plt.show()
-
-# plt.plot(data["time_step"], data["accuracy_normal"])
-# plt.show()
+    plot_accuracy_graph(data, "paths", "time", "Time", "Amount of paths",
+                        "Time")
+    plt.show()
