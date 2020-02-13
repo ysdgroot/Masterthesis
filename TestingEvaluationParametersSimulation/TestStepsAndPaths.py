0c20ad408950b8effb1b88a120dcc357d6f4f7ce
ysdgroot <yucatan.de.groote@gmail.com>
2020-02-13 10:47:14 +0100

    Aanpassing voor het bekijken van de andere opties. Om op de vraag te kunnen beantwoorden wat het verband is van volatitiliet tussen de optie prijs en voor het aantal paden en stappen per maturiteit
    Signed-off-by: ysdgroot <yucatan.de.groote@gmail.com>

--- TestingEvaluationParametersSimulation/TestStepsAndPaths.py
+++ TestingEvaluationParametersSimulation/TestStepsAndPaths.py
@@ -1,8 +1,11 @@
 from ModelsStock.BlackScholes import BlackScholes
 from OptionModels.PlainVanilla import PlainVanilla
+from OptionModels.EuropeanAsian import AsianMean
+from OptionModels.EuropeanLookback import Lookback
 import time
 import csv
 from joblib import Parallel, delayed
+import numpy as np
 
 # Testing paths
 time_steps_per_maturities = [i for i in range(100, 1001, 100)]
@@ -10,19 +13,28 @@ amount_paths = [i for i in range(1000, 20001, 1000)]
 add_header = True
 write_comment_info = True
 
-file_name = 'Test-steps and accuracy-BS-v1.csv'
+file_name = 'Test-steps and accuracy-BS-v2-Asian.csv'
 maturity = 10
 interest_rate = 0.001
 volatitlity = 0.1
 start_price = 100
 strike_price = 100
 
-number_iterations = 20
+number_iterations = 50
 
 BS = BlackScholes(interest_rate, volatitlity)
-option = PlainVanilla()  # standard European Option
+# option = PlainVanilla()  # standard European Option
+# option_name = "Plain vanilla"
+is_plain_vanilla = False
 
-exact_call = BlackScholes.solution_call_option(start_price, strike_price, maturity, interest_rate, volatitlity)
+option = AsianMean()  # Asian option
+option_name = "Asian mean"
+
+# option = Lookback(lookback_min=True)
+# option_name = "Lookback_min"
+
+if is_plain_vanilla:
+    exact_call = BlackScholes.solution_call_option(start_price, strike_price, maturity, interest_rate, volatitlity)
 
 # write some comments about the test
 if write_comment_info:
@@ -32,13 +44,16 @@ if write_comment_info:
         fd.write("# Volatility = {} \n".format(volatitlity))
         fd.write("# Start_price = {} \n".format(start_price))
         fd.write("# Strike_price = {} \n".format(strike_price))
-        fd.write("# Option = Plainvanilla \n")
+        fd.write("# Option = {} \n".format(option_name))
 
 # write header (if necessary)
 if add_header:
     # col_names = ['time_step', 'paths', 'time', 'accuracy_absolute', 'accuracy_normal', 'max_abs_diff', 'min_rel_diff',
     #              'max_rel_diff', 'maturity', 'interest_rate', 'volatility', 'exact_value']
-    col_names = ['time_step', 'paths', 'time', 'accuracy_absolute', 'accuracy_normal', 'exact_value']
+    if is_plain_vanilla:
+        col_names = ['time_step', 'paths', 'time', 'accuracy_absolute', 'accuracy_normal', 'exact_value']
+    else:
+        col_names = ['time_step', 'paths', 'time', 'option_price']
     with open(file_name, 'a', newline='') as fd:
         csv.writer(fd).writerow(col_names)
 
@@ -54,12 +69,19 @@ def func_time_step(time_step, amount):
         total_time = end - start
 
         approx_call = option.get_price(paths, maturity, interest_rate, strike_price=strike_price)
-        # absolute_rel_diff += abs(exact_call - approx_call) / exact_call
-        # rel_diff += (approx_call - exact_call) / exact_call
-        rel_diff_abs = abs(exact_call - approx_call) / exact_call
-        rel_diff = (approx_call - exact_call) / exact_call
+        temp_result = [time_step, amount, total_time]
 
-        temp_result = [time_step, amount, total_time, rel_diff_abs, rel_diff, exact_call]
+        if is_plain_vanilla:
+            rel_diff_abs = abs(exact_call - approx_call) / exact_call
+            rel_diff = (approx_call - exact_call) / exact_call
+
+            # adding the additional information in case if is a plainvanilla option.
+            # In this case we can compare the result with the theoretical price of the option.
+            temp_result.extend([rel_diff_abs, rel_diff, exact_call])
+        else:
+            # adding the price of the simulated option
+            temp_result.append(approx_call)
+
         with open(file_name, 'a', newline='') as fd:
             csv.writer(fd).writerow(temp_result)
 
