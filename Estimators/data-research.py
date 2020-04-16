4c11526e621e543b325bef7751a522ac86391fab
ysdgroot <yucatan.de.groote@gmail.com>
2020-04-16 17:17:28 +0200

    Comments and cleaning code
    Signed-off-by: ysdgroot <yucatan.de.groote@gmail.com>

--- Estimators/data-research.py
+++ Estimators/data-research.py
@@ -1,57 +1,63 @@
 import Estimators.preprocessing_data as prep
-import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# todo: schrijven van commentaar
-# het doel van deze file is het algemeen beschrijven van de gegeneeerde data
 def difference_exact_vs_simulation():
+    """
+    This function is to check the difference between the simulations of the data and the exact theoretic value.
+    In this case it means the difference of the option price based on the Black-Scholes model.
+    """
+
     def get_n_smallest_elements(dataframe_column, amount):
+        # returns the list of the 'amount' smallest elements from column in a Dataframe
         list_data = dataframe_column.tolist()
         list_data.sort()
         return list_data[0:amount]
 
     def get_n_greatest_elements(dataframe_column, amount):
+        # returns the list of the 'amount' largest elements from column in a Dataframe
         list_data = dataframe_column.tolist()
         list_data.sort(reverse=True)
         return list_data[0:amount]
 
     datamanager = prep.DataManager("BS")
+    file_name_v2 = datamanager.get_correct_file(first_version=False, test=False, model="BS")
+    df_v2 = pd.read_csv(file_name_v2, comment='#', header=0)
 
-    file_name_v1 = datamanager.get_correct_file(first_version=False, test=False, model="BS")
+    diff_v2 = df_v2["opt_exact_standard"] - df_v2["opt_standard"]
+    min_diff_v2 = get_n_smallest_elements(diff_v2, 10)
+    max_diff_v2 = get_n_greatest_elements(diff_v2, 10)
 
+    print("Second version; data is sampled with maturity between 1 and 25")
+    print("Results = Theoretic - Simulation")
+
+    print("Biggest diffence, where simulation estimates to large")
+    for index, diff in enumerate(min_diff_v2):
+        print(f"{index}: {diff}")
+
+    print("Biggest diffence, where simulation estimates to small")
+    for index, diff in enumerate(max_diff_v2):
+        print(f"{index}: {diff}")
+
+    # -----------------------------------------------------------------------------------------------------------------
+    file_name_v1 = datamanager.get_correct_file(first_version=True, test=False, model="BS")
     df_v1 = pd.read_csv(file_name_v1, comment='#', header=0)
 
     diff_v1 = df_v1["opt_exact_standard"] - df_v1["opt_standard"]
-    min_diff_v1 = get_n_smallest_elements(diff_v1, 1000)
+    max_diff_v1 = get_n_greatest_elements(diff_v1, 10)
+    min_diff_v1 = get_n_smallest_elements(diff_v1, 10)
+    print("First version; data is sampled with maturity between 1 and 60")
+    print("Results = Theoretic - Simulation")
+
+    print("Biggest diffence, where simulation estimates to large")
     for index, diff in enumerate(min_diff_v1):
         print(f"{index}: {diff}")
 
-    file_name_v1_vg = datamanager.get_correct_file(first_version=True, test=False, model="VG")
-    df_v1_vg = pd.read_csv(file_name_v1_vg, comment='#', header=0)
-
-    max_prices = get_n_greatest_elements(df_v1_vg["opt_standard"], 50)
-    print("Maximum pricing")
-    for index, diff in enumerate(max_prices):
+    print("Biggest diffence, where simulation estimates to small")
+    for index, diff in enumerate(max_diff_v1):
         print(f"{index}: {diff}")
 
-    # full_dataframe_train = datamanager.get_full_dataframe(test_data=False)
-    # full_dataframe_test = datamanager.get_full_dataframe(test_data=True)
-    #
-    # difference_train = full_dataframe_train["opt_exact_standard"] - full_dataframe_train["opt_standard"]
-    # difference_test = full_dataframe_test["opt_exact_standard"] - full_dataframe_test["opt_standard"]
-    #
-    # min_diff_train = get_n_smallest_elements(difference_train, 30)
-    # print(min_diff_train)
-    #
-    # # plt.hist(difference_train)
-    # # plt.show()
-    #
-    # min_diff_test = get_n_smallest_elements(difference_test, 20)
-    # print(min_diff_test)
-
-    # plt.hist(difference_test)
-    # plt.show()
+    # -----------------------------------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
