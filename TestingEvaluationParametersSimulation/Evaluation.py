4aaa0f913aa5c0c14c48aaf4f7ab1b039d5ad230
ysdgroot <yucatan.de.groote@gmail.com>
2020-04-25 07:01:25 +0200

    Name change and made figures in Dutch
    Signed-off-by: ysdgroot <yucatan.de.groote@gmail.com>

--- TestingEvaluationParametersSimulation/Evaluation.py
+++ TestingEvaluationParametersSimulation/Evaluation.py
@@ -11,6 +11,7 @@ dict_model_names = {"BS": "Black Scholes",
 
 # Which option_types that needs to be evaluated 'Standard', 'Asian','Lookback'
 option_names = ["Standard", "Asian", "Lookback"]
+dict_option_names_nl = dict(zip(option_names, ["Standaard", "Aziatische", "Lookback"]))
 evaluate_options = [True, True, True]
 
 plot_mean = True
@@ -24,29 +25,29 @@ percentile = 1
 restriction = None
 
 # column name from the csv file as the X-variable
-# x_name = "paths"
-x_name = "time_step"
+x_name = "paths"
+# x_name = "time_step"
 
 # column name from de csv file as Y-variable
 y_name = "option_price"
-y_label = "Price option"
+y_label = "Prijs optie"
 
 y_name_standard_BS = "relative_diff"
 # y_name_standard_BS = "absolute_diff"
-dict_y_label_standardBS = {"relative_diff": "Relative difference",
-                           "absolute_diff": "Absolute Relative difference"}
+dict_y_label_standardBS = {"relative_diff": "Relatief verschil",
+                           "absolute_diff": "Absoluut relatief verschil"}
 
-dict_label_name = {"paths": "Number of paths",
-                   "time_step": "Number of steps per maturity"
+dict_label_name = {"paths": "Aantal paden",
+                   "time_step": "Aantal stappen per maturiteit"
                    }
 x_label = dict_label_name[x_name]
 
 # Standard title of the plot, first the option type and then the Stock model
-title_plot = "Variance price {} option - {}"
-title_plot_standard_BS = "Performance Simulation vs Theory - {}"
+title_plot = "Variantie prijs {} optie - {}"
+title_plot_standard_BS = "Prestatie Simulatie vs Theorie - {}"
 
-dict_title_restriction = {"paths": "-Paths=({},{})",
-                          "time_step": "-Steps=({},{})"
+dict_title_restriction = {"paths": "-Paden=({},{})",
+                          "time_step": "-Stappen=({},{})"
                           }
 
 
@@ -56,7 +57,7 @@ def read_data(model, option_type):
     Read and return the data from a model and option type.
 
     :param model: str (BS, VG or H)
-    :param option_type: str, ("Standard", "Asian", "Lookback(min)", "Lookback(max)")
+    :param option_type: str, ("Standard", "Asian", "Lookback")
     :return: pd.Dataframe (columns depends on the model)
     """
     filename = get_filename(model, option_type)
@@ -133,28 +134,28 @@ def plot_change_variance(data, x_name, y_name, title, xlabel, ylabel, plot_min_m
 
         # the lines are the same for the minimum and maximum percentile
         legend_variables.append(line_min_percentile)
-        legend_variable_names.append(f"Percentiles {percentile} - {percentile_max}")
+        legend_variable_names.append(f"Percentiel {percentile} - {percentile_max}")
 
     if plot_mean:
         mean_line = []
         for x in unique_x:
             mean_line.append(np.mean(data_y[data[x_name] == x]))
-        line_mean, = plt.plot(unique_x, mean_line, color='red')
+        line_mean, = plt.plot(unique_x, mean_line, color='black')
 
         legend_variables.append(line_mean)
-        legend_variable_names.append("Mean")
+        legend_variable_names.append("Gemiddelde")
 
+    if x_name == "time_step":
+        restrictions = [("paths", 1000, 5000), ("paths", 6000, 10000), ("paths", 11000, 15000), ("paths", 16000, 20000)]
+    else:
+        restrictions = [("time_step", 5, 50), ("time_step", 55, 100), ("time_step", 200, 600), ("time_step", 700, 1000)]
     # todo deze stuk code verwijderen
-    restrictions = [("paths", 1000, 10000), ("paths", 10000, 20000)]
-    # restrictions = [("paths", i, i + 1000) for i in range(1000, 20000, 1000)]
-    # restrictions = [("time_step", 5, 100), ("time_step", 200, 600), ("time_step", 700, 1000)]
-    # # ("time_step", 101, 300), ("time_step", 301, 600),, ("time_step", 801, 1000), ("time_step", 55, 100), ("time_step", 101, 300),
     for restrict in restrictions:
         scat_points = plt.scatter(data_x[(restrict[1] <= data[restrict[0]]) & (data[restrict[0]] <= restrict[2])],
                                   data_y[(restrict[1] <= data[restrict[0]]) & (data[restrict[0]] <= restrict[2])])
 
-        dict_rename = {"time_step": "n_steps",
-                       "paths": "n_paths"}
+        dict_rename = {"time_step": "#stappen",
+                       "paths": "#paden"}
 
         legend_variables.append(scat_points)
         legend_variable_names.append(f"{dict_rename[restrict[0]]}: [{restrict[1]}-{restrict[2]}]")
@@ -250,7 +251,7 @@ if any(evaluate_options) and any(evaluate_options):
                     plot_change_variance(data_options,
                                          x_name,
                                          y_name,
-                                         title=title_plot.format(option, dict_model_names[model]),
+                                         title=title_plot.format(dict_option_names_nl[option], dict_model_names[model]),
                                          xlabel=x_label,
                                          ylabel=y_label,
                                          plot_mean=plot_mean,
@@ -258,4 +259,3 @@ if any(evaluate_options) and any(evaluate_options):
                                          plot_percentile=plot_percentile,
                                          percentile=percentile,
                                          restriction=restriction)
-
