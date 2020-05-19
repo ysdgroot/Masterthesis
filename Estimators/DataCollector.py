d9317c7cbeb62faa869ff12e0c3437eba1789512
ysdgroot <yucatan.de.groote@gmail.com>
2020-05-19 15:30:21 +0200

    get real option prices and small changes
    Signed-off-by: ysdgroot <yucatan.de.groote@gmail.com>

--- Estimators/DataCollector.py
+++ Estimators/DataCollector.py
@@ -6,23 +6,22 @@ import Data
 
 class DataManager:
 
-    def __init__(self, model="BS",
-                 column_fitting="opt_standard",
+    def __init__(self, stockmodel="BS",
+                 option_type="opt_standard",
                  replace_call_put=True,
                  call_put=(1, -1),
                  only_call=False,
                  list_column_names=None,
                  shuffle_data=False,
-                 with_percent=False,
-                 first_version=False):
+                 with_percent=False):
         """
-        :param model:str (default="BS", values in ["BS", "VG", "H"]
-        :param column_fitting: str (default="opt_standard"), standard values are
+        :param stockmodel:str (default="BS", values in ["BS", "VG", "H"]
+        :param option_type: str (default="opt_standard"), standard values are
                                                 ["opt_standard",
                                                 "opt_asianmean",
                                                 "opt_lookbackmin",
                                                 "opt_lookbackmax"]
-                                    for the BS model, there is the possibility of "opt_exact_standard"
+                                    for the BS stockmodel, there is the possibility of "opt_exact_standard"
         :param replace_call_put: bool (default=True), if the column "call/put" needs to be changed
         :param call_put: 2-tuple (default=(1,-1)), when replace_call_put=True, then it will be changed in these values.
                                                 first value is the 'call', second the 'put'
@@ -30,7 +29,6 @@ class DataManager:
         :param list_column_names: list[str] (default=None), if a specific column set is necessary as training data.
                                 If None, all the columns will be given.
         :param shuffle_data: bool (default=False), whenever the data needs to be shuffled.
-        :param first_version: bool (default=False), whenever to use the old simulated data.
         """
         self.replace_call_put = replace_call_put
         self.call_put = call_put
@@ -63,19 +61,19 @@ class DataManager:
                                       "VG": [],
                                       "H": []}
 
-        if not model in self.possible_models:
-            raise ValueError(f'{model} is not a possible modelname')
-        self.model = model
+        if not stockmodel in self.possible_models:
+            raise ValueError(f'{stockmodel} is not a possible modelname')
+        self.model = stockmodel
 
-        self.column_fitting = column_fitting
-        self.set_column_fitting(column_fitting)
+        self.column_fitting = option_type
+        self.set_column_fitting(option_type)
 
         self.list_column_names = None
         self.set_column_names(list_column_names=list_column_names)
 
         # get filenames
-        self.training_file_name = self.get_correct_file(self.model, test=False, first_version=first_version)
-        self.test_file_name = self.get_correct_file(self.model, test=True, first_version=first_version)
+        self.training_file_name = self.get_correct_file(self.model, test=False)
+        self.test_file_name = self.get_correct_file(self.model, test=True)
 
         self.only_call = only_call
         self.shuffle_data = shuffle_data
@@ -97,23 +95,22 @@ class DataManager:
             df.loc[df["call/put"] == 'P', "call/put"] = call_put[1]
 
     @staticmethod
-    def get_correct_file(model, test=False, first_version=False):
+    def get_correct_file(model, test=False):
         """
         Get the correct file_name where the data is stored.
 
         :param model: str, to get the correct file_name
         :param test: bool (default: False), if the file is test data or not
-        :param first_version: bool (default: False), whenever to use the older version of the data
         :return: full path name of the file
         """
         try:
             file = pkg_resources.open_text(GeneratedData,
-                                           f"generateddata-{model}-{'testing' if test else 'training'}"
-                                           f"{'' if first_version else '-v2'}.csv")
+                                           f"generateddata-{model}-"
+                                           f"{'testing' if test else 'training'}.csv")
             # data_folder = Path('C:/Users/Yucatan De Groote/Documents/Universiteit/Masterproef/GeneratedData')
             return file.name
         except FileNotFoundError:
-            print("File not found: Incorrect model name")
+            print("File not found: Incorrect stockmodel name")
             return None
 
     def get_only_call(self, only_call=True):
@@ -191,18 +188,17 @@ class DataManager:
 
         return training_data_x, training_data_y
 
-    def get_random_training_data(self, n_samples=10000, random_state=8843):
+    def get_random_training_data(self, n_samples=10000, random_state=8843, get_not_selected_data=False):
         """
         Function to get random samples from the full dataset.
         :param n_samples: int, number of datapoints
         :param random_state: int, for the randomstate.
+        :param get_not_selected_data: bool (default=False), to get the data which are not from the random selected values.
         :return: (X, y), pandas.dataframe,
                         X: dataframe with all the columns to train the models (or only the list_column_names)
                         y: dataframe (just one column) with the results of the X values.
         """
         training_data = pd.read_csv(self.training_file_name, comment='#', header=0)
-
-        training_data = training_data.sample(n=n_samples, random_state=random_state)
 
         # replacing the values for call-put
         self.func_replace_call_put(training_data)
@@ -213,16 +209,27 @@ class DataManager:
         # getting the positions in case only the calls are asked
         positions_call = training_data["call/put"] == self.call_put[0]
 
-        training_data_x = training_data[columns_x] if not self.only_call \
-            else training_data[columns_x][positions_call]
+        # select only the call options
+        if self.only_call:
+            training_data = training_data[positions_call]
 
-        training_data_y = training_data[columns_y] if not self.only_call \
-            else training_data[columns_y][positions_call]
+        training_data_sample = training_data.sample(n=n_samples, random_state=random_state)
+        training_data_sample_index = training_data_sample.index
 
         if self.list_column_names is not None:
-            training_data_x = pd.DataFrame(training_data_x, columns=self.list_column_names)
+            training_data_x = pd.DataFrame(training_data_sample, columns=self.list_column_names)
+        else:
+            training_data_x = training_data_sample[columns_x]
 
-        return training_data_x, training_data_y
+        training_data_y = training_data_sample[columns_y]
+
+        if get_not_selected_data:
+            not_selected_x = training_data.loc[set(training_data.index) - set(training_data_sample_index)][columns_x]
+            not_selected_y = training_data.loc[set(training_data.index) - set(training_data_sample_index)][columns_y]
+
+            return training_data_x, training_data_y, not_selected_x, not_selected_y
+        else:
+            return training_data_x, training_data_y
 
     def get_test_data(self):
         """
@@ -256,7 +263,7 @@ class DataManager:
         """
         Function to get the full dataframe of the training or test file.
         :param test_data: boolean (default=False), whenever to get the test(=True) data of the training(=False) data
-        :return: pandas.Dataframe with the training/test data, with all the columns of the model from the declaration
+        :return: pandas.Dataframe with the training/test data, with all the columns of the stockmodel from the declaration
         """
         file_name = self.test_file_name if test_data else self.training_file_name
         data = pd.read_csv(file_name, comment='#', header=0)
@@ -269,48 +276,92 @@ class DataManager:
 
 class DataManagerRealData:
 
-    @staticmethod
-    def get_file_name(symbol):
+    def __init__(self, symbol="SPX", test_month=9):
         """
-        # todo: commentaar
-        :param symbol:
-        :return:
+        :param symbol: str, ("SPX", "SPXPM" or "SX5E) (default="SPX")
+        :param test_month: the month used as test data
         """
+        self.symbol = symbol
+        self.test_month = test_month
+        self.columns_x = ["strike",
+                          "call_put",
+                          "stock_price_for_iv",
+                          "number_days"]
+        self.columns_y = ["mean_price"]
+
+    def get_file_name(self):
+        """
+        :return: str or None, The full name (adress) of the file
+                if None the file is not found,
+        """
+        symbol = self.symbol
         try:
-            file = pkg_resources.open_text(Data, f"Real data {symbol}.csv")
-            # data_folder = Path('C:/Users/Yucatan De Groote/Documents/Universiteit/Masterproef/GeneratedData')
+            # file = pkg_resources.open_text(Data, f"Real data {symbol}.csv")
+            file = pkg_resources.open_text(Data, f"{symbol}.csv")
             return file.name
         except FileNotFoundError:
             print("File not found")
             return None
 
-    def get_data(self, symbol="SPX"):
-        # todo dit aanvullen
-        # style of the file is only E (European option)
-        # "stock_price_for_iv"
-        # "option_expiration"
-        # "date"
-        # "symbol"
-        columns_x = ["stock_price_close",
-                     "strike", "call_put",
-                     "open_interest"]
-        columns_y = ["mean_price"]
+    def get_filter(self):
+        """
+        Function to get the start period and end period of the month (self.test_month)
 
-        important_col = ["date", "option_expiration", "symbol"] + columns_x + columns_y
+        :return:(begin, end), both Timestamps of the test month
+        """
+        first_check = pd.Timestamp(year=2014, month=self.test_month, day=1)
 
-        pd.set_option('display.expand_frame_repr', False)
-        df = pd.read_csv(self.get_file_name(symbol), header=0, usecols=important_col)
+        if self.test_month != 12:
+            second_check = pd.Timestamp(year=2014, month=self.test_month + 1, day=1)
+        else:
+            second_check = pd.Timestamp(year=2015, month=1, day=1)
 
-        # print(df.info())
+        return first_check, second_check
 
-        df.loc[df["call_put"] == 'C', "call_put"] = 1
-        df.loc[df["call_put"] == 'P', "call_put"] = -1
+    def get_training_data(self):
+        """
+        :return: (X, y) of the training data.
+            All the datapoints but a month (`test_month')
+        """
 
-        df["date"] = pd.to_datetime(df['date'], format='%m/%d/%Y')
-        df["option_expiration"] = pd.to_datetime(df["option_expiration"], format='%m/%d/%Y')
+        df = pd.read_csv(self.get_file_name(), header=0, parse_dates=["date"])
 
-        df["number_days"] = df.apply(lambda row: (row.option_expiration - row.date).days, axis=1)
+        first_check, second_check = self.get_filter()
 
-        columns_x.append("number_days")
+        filter = (df["date"] >= first_check) & (df["date"] < second_check)
 
-        return df[columns_x][df["symbol"] == symbol], df[columns_y][df["symbol"] == symbol]
+        return df[self.columns_x][-filter], df[self.columns_y][-filter]
+
+    def get_test_data(self):
+        """
+        :return: (X, y) of the test data.
+            The test data is a specific month
+        """
+
+        df = pd.read_csv(self.get_file_name(), header=0, parse_dates=["date"])
+
+        first_check, second_check = self.get_filter()
+
+        filter = (df["date"] >= first_check) & (df["date"] < second_check)
+
+        return df[self.columns_x][filter], df[self.columns_y][filter]
+
+    def get_random_training_test_data(self, n_samples, random_state=3477):
+        """
+        Takes random samples of the training and test data
+        :param n_samples: int, amount of datapoints for the training data
+        :return: (X_train, y_train, X_test, y_test)
+        """
+
+        df = pd.read_csv(self.get_file_name(), header=0, parse_dates=["date"])
+
+        training_data = df.sample(n_samples, random_state=random_state)
+
+        training_data_sample_index = training_data.index
+
+        test_data = df.loc[set(df.index) - set(training_data_sample_index)]
+
+        return training_data[self.columns_x], training_data[self.columns_y], test_data[self.columns_x], test_data[
+            self.columns_y]
+
+    # todo: verwijderen
